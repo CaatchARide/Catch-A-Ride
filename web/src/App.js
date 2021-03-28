3b498d99f34d3f5deb253d6e26819cdc31f6dece
salashector99 <47862605+salashector99@users.noreply.github.com>
2021-03-28 19:39:50 -0400

    Updating src files 3/28/21

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,15 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import login from './components/pages/Login'
 import signup from './components/pages/Signup'
+import about from './components/pages/About'
+import car_details from './components/pages/car_details'
+import switchpage from './components/pages/Switchpage'
+import contact from './components/pages/Contact'
+import driverDashboard from './components/pages/driverDashboard'
+import passengerDashboard from './components/pages/passengerDashboard'
+
+
+
 //import {Button, Form, Container} from 'react-bootstrap'
 
 function App() {
@@ -14,6 +23,13 @@ function App() {
       <Switch>
         <Route path = '/login' exact component= {login}/>
         <Route path = '/signup' exact component= {signup}/>
+        <Route path = '/about' exact component= {about}/>
+        <Route path = '/car_details' exact component= {car_details}/>
+        <Route path = '/switchpage' exact component= {switchpage}/>
+        <Route path = '/Contact' exact component= {contact}/>
+        <Route path = '/driverDash' exact component= {driverDashboard}/>
+        <Route path = '/passengerDash' exact component= {passengerDashboard}/>
+
       </Switch>
       </Router>
     </>
