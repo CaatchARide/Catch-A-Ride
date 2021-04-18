e4aba1d510367aa2d4301097c3e42a1d956c900e
RobertUAGreatDanes <57924452+RobertUAGreatDanes@users.noreply.github.com>
2021-04-18 11:58:47 -0400

    Add files via upload
    Sample code for checkout

---
+++ server.js
@@ -0,0 +1,32 @@
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
