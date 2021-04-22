cd3ba1f6c6e13062d1da660cbcb3fffe315b5bb1
RobertUAGreatDanes <57924452+RobertUAGreatDanes@users.noreply.github.com>
2021-04-21 20:54:17 -0400

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,28 @@
+import React from "react";
+import "./styles.css";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
-import './App.css';
+const stripePromise = loadStripe("pk_test_35p114pH8oNuHX72SmrvsFqh00Azv3ZaIA");
 
-function App() {
+const App = () => {
   return (
-    
+    <div className="App">
+      <div className="product">
+        <img
+          src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress"
+          alt="laptop"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
