e0762e13b6e96cf1db5963d0890bb01e229f86fb
salashector99 <47862605+salashector99@users.noreply.github.com>
2021-04-30 15:52:18 -0400

    Add files via upload

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 import home from './components/pages/Home'
 import login from './components/pages/Login'
 import signup from './components/pages/Signup'
 import about from './components/pages/About'
-import car_details from './components/pages/car_details'
+import car_details from './components/pages/car_details.js'
 import switchpage from './components/pages/Switchpage'
 import contact from './components/pages/Contact'
 import driverDashboard from './components/pages/driverDashboard'
 import passengerDashboard from './components/pages/passengerDashboard'
+import post from './components/pages/Post'
+import Footer from './components/Footer'
+import payment from './components/pages/payment/payment'
+import request from './components/pages/Request'
 
 
 
@@ -32,8 +39,11 @@ function App() {
         <Route path = '/Contact' exact component= {contact}/>
         <Route path = '/driverDash' exact component= {driverDashboard}/>
         <Route path = '/passengerDash' exact component= {passengerDashboard}/>
-
+        <Route path = '/posts' exact component= {post}/>
+        <Route path = '/payment' exact component= {payment}/>
+        <Route path = '/request' exact component= {request}/>
       </Switch>
+      <Footer />
       </Router>
     </>
   );
