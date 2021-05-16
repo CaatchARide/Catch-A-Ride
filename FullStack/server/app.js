bfa5475f39c64d8e20c155a1d4a9449524961c54
Varun <varunchandan1999@gmail.com>
2021-05-16 00:15:33 -0400

    Co-authored-by: salashector99 <salashector99@users.noreply.github.com> Co-authored-by: Josh Forcier <JoshForce@users.noreply.github.com> Co-authored-by: tiannabr <tiannabr@users.noreply.github.com> Co-authored-by: Mornan, Rob C <rmornan@albany.edu>

--- FullStack/server/app.js
+++ FullStack/server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./models/userSchema');
-const dotenv = require("dotenv");
-//const passport = require('passport');
+const dotenv = require('dotenv');
+const session = require('express-session');
+const MongoDbSession = require('connect-mongodb-session')(session);
+const cookieParser = require('cookie-parser')
+
+//const MongoStore = require('connect-mongo');
+
+const passport = require('passport');
+const path = require('path');
+
 //const flash = require('express-flash');
 
 //passport
@@ -11,7 +19,7 @@ const dotenv = require("dotenv");
     //email => users.find(user => user.email === email)
 //)
 
-//const users = []
+
 
 //  routes
 const userRoutes = require('./routes/auth');
@@ -19,21 +27,26 @@ const contactRoutes = require('./routes/contact');
 const tripRoutes = require('./routes/Trip');
 const requestRoutes = require('./routes/request')
 const chatRoutes = require('./routes/chat');
+const detailsRoutes = require('./routes/driver');
+
 
 const app = express();
 
 
-// Use the router file
-app.use(require('./routes/auth', userRoutes));
-app.use(require('./routes/contact', contactRoutes));
-app.use(require('./routes/Trip', tripRoutes));
-app.use(require('./routes/request', requestRoutes));
-app.use(require('./routes/chat', chatRoutes));
+
+
+
 
 
 
 // BodyParser Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(cookieParser());
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // connect to MongoDB
@@ -49,13 +62,40 @@ mongoose.connect(DB, {
 })
 .catch(err => console.log(err));
 
+// Express session 
+const store = new MongoDbSession({
+    uri: process.env.DATABASE,
+    collection: "userSessions"
+
+})
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+
+
+}))
+
+
+// Use the router file
+app.use(require('./routes/auth', userRoutes));
+app.use(require('./routes/contact', contactRoutes));
+app.use(require('./routes/Trip', tripRoutes));
+app.use(require('./routes/request', requestRoutes));
+app.use(require('./routes/chat', chatRoutes));
+app.use(require('./routes/driver', detailsRoutes));
 
 
 
 
 
-
-
+// Bring in the passport authentication strategy 
+require('./middleware/passport-config')(passport);
 
 
 const PORT = process.env.PORT || 9000;
